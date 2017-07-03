71e9471190b6008e49adb69b1479526af8060409
Rachel Traina-Grandon <rtraina-grandon@hitactics.com>
2017-07-03 16:46:16 -0700

    Added application components, created global styles, and bundled.

---
+++ app/components/footer.component.js
@@ -0,0 +1,15 @@
+import React from 'react';
+// import ClassNames from 'classnames';
+
+class Footer extends React.Component {
+  render(){
+    return(
+      <div className="footer">
+        <p>designs <i className="fa fa-times" aria-hidden="true"></i>by traina</p>
+      </div>
+    )
+  }
+
+}
+
+export default Footer
